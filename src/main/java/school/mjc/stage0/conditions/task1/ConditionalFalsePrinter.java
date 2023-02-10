e1f260359a5f568270ac59c0942a2d84f678f3d4
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-10 16:31:38 +0500

    hi

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean b = false;
+
+        if(b==true){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean b = true;
+
+        if(b==true){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
     }
 }
